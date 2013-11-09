f10caf404940c222aec3f45587ab62a91337705e
Patrick Kimes <pkimes@live.unc.edu>
2013-11-09 19:48:15 +0000

    Correct orientation of data input to sigclust()
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/SigFuge@83121 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/SFpval.R
+++ R/SFpval.R
@@ -1,5 +1,6 @@
 SFpval <- function(data, normalize = 1, flag = 1) {
 
+<<<<<<< HEAD
   # normalize data
   if (normalize == 1) {
     data <- SFnormalize(data, flag)$data.norm
@@ -11,5 +12,18 @@ SFpval <- function(data, normalize = 1, flag = 1) {
       # nrep=1 appears to produre inconsistent results
 
   return(sc)
+=======
+    # normalize data
+    if (normalize == 1) {
+        data <- SFnormalize(data, flag)$data.norm
+    }
+
+    # calculate sigclust p-value on normalized data
+    data <- log10(data+1)
+    sc <- sigclust::sigclust(t(data), nsim=100, nrep=2, icovest=2)
+        # nrep=1 appears to produre inconsistent results
+  
+    return(sc)
+>>>>>>> Correct orientation of data input to sigclust()
 
 }
