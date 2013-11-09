70330c428c63589b4eec769c57a6c18733474e7b
Patrick Kimes <pkimes@live.unc.edu>
2013-11-09 21:12:54 +0000

    revert to correct files
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/SigFuge@83135 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/SFpval.R
+++ R/SFpval.R
@@ -1,9 +1,5 @@
 SFpval <- function(data, normalize = 1, flag = 1) {
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> change indents to 2 spaces
   # normalize data
   if (normalize == 1) {
     data <- SFnormalize(data, flag)$data.norm
@@ -15,21 +11,5 @@ SFpval <- function(data, normalize = 1, flag = 1) {
       # nrep=1 appears to produre inconsistent results
 
   return(sc)
-<<<<<<< HEAD
-=======
-    # normalize data
-    if (normalize == 1) {
-        data <- SFnormalize(data, flag)$data.norm
-    }
-
-    # calculate sigclust p-value on normalized data
-    data <- log10(data+1)
-    sc <- sigclust::sigclust(t(data), nsim=100, nrep=2, icovest=2)
-        # nrep=1 appears to produre inconsistent results
-  
-    return(sc)
->>>>>>> Correct orientation of data input to sigclust()
-=======
->>>>>>> change indents to 2 spaces
 
 }
